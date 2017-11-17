8b80c1b84493c7d50f3e934f914438eaff3c70cd
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-11-17 12:13:48 -0500

    backup

--- Pixel_Invader_Unity/Assets/Scripts/LevelBuilder.cs
+++ Pixel_Invader_Unity/Assets/Scripts/LevelBuilder.cs
@@ -81,7 +81,6 @@ public class LevelBuilder : MonoBehaviour {
                 }
             }
             GameManager.instance.currentLineEnemyDirection *= -1;
-            Debug.Log(GameManager.instance.currentLineEnemyDirection);
         }
     }
 
