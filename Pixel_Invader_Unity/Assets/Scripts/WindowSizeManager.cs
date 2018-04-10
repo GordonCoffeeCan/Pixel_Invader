c10ba9acf79e3277ad8364d3c0b59435178bad07
Unknown <gordon.lee.6170@gmail.com>
2018-04-09 21:26:25 -0400

    window resize

---
+++ Pixel_Invader_Unity/Assets/Scripts/WindowSizeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowSizeManager : MonoBehaviour {
+
+    public static WindowSizeManager instance;
+
+    [HideInInspector] public Vector2 halfWindowSize = Vector2.zero;
+
+    private void Awake() {
+        instance = this;
+        halfWindowSize = Resize();
+    }
+
+    private Vector2 Resize() {
+        Vector2 _halfWindowSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        return _halfWindowSize;
+    }
+}
